8847636aa79098e62d509c08ccac7a4679c39af0
Pierre Gambarotto <pierre.gambarotto@math.univ-toulouse.fr>
2019-12-02 12:32:11 +0100

    format source with hfmt

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,23 +1,25 @@
-module Day1 (main) where
+module Day1
+  ( main
+  ) where
 
-import           Paths_aoc2019         (getDataFileName)
+import           Paths_aoc2019 (getDataFileName)
 
 type Mass = Integer
 
 type Fuel = Integer
 
 fuelModule :: Mass -> Fuel
-fuelModule m = (m `div` 3) -2
-
+fuelModule m = (m `div` 3) - 2
 
 fuelTotal :: (Mass, Fuel) -> (Mass, Fuel)
-fuelTotal (n,f)
-  | n<9 = (0,f)
-  | otherwise = fuelTotal(fm,f + fm) where
-      fm = fuelModule n
+fuelTotal (n, f)
+  | n < 9 = (0, f)
+  | otherwise = fuelTotal (fm, f + fm)
+  where
+    fm = fuelModule n
 
 parseInput :: String -> [Mass]
-parseInput  = map read . lines
+parseInput = map read . lines
 
 loadInput :: IO String
 loadInput = getDataFileName "inputs/day1.txt" >>= readFile
@@ -29,4 +31,4 @@ day1 :: IO ()
 day1 = do
   input <- fmap parseInput loadInput
   print $ foldr (+) 0 $ map fuelModule input
-  print $ foldr (+) 0 $ map (snd . fuelTotal . (\x -> (x,0))) input 
+  print $ foldr (+) 0 $ map (snd . fuelTotal . (\x -> (x, 0))) input
